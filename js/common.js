c6ddd6731956b41ed7f2b8de17180fd0b76f5dd0
beaverok <askgroup77@gmail.com>
2016-09-19 17:06:52 +0500

    add project page files

--- js/common.js
+++ js/common.js
@@ -31,5 +31,23 @@ $(document).ready(function () {
 
     $('.page-header__menu-btn').click(function () {
         $('.main-menu').slideToggle();
-    })
+    });
+    $(document).click(function(event) {
+        if ($(window).width() < 767 && $('.main-menu:visible')) {
+            if ($(event.target).closest(".page-header").length) return;
+            $('.main-menu').slideUp();
+            event.stopPropagation();
+        }
+    });
+
+    $('.page-header__projects-active').click(function () {
+       $('.page-header__projects-list').slideToggle();
+    });
+    $(document).click(function(event) {
+        if ($('.page-header__projects-list:visible')) {
+            if ($(event.target).closest(".page-header").length) return;
+            $('.page-header__projects-list').slideUp();
+            event.stopPropagation();
+        }
+    });
 });
